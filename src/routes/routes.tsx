a51a1e7e4568ab81e0c94a2310dee42d90f5dbad
Shariful4447 <shariful.islam4447@gmail.com>
2024-09-13 22:32:56 +0200

    Different pages create and set path

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+  },
+  {
+    path: "/About",
+    element: <About></About>,
+  },
+  {
+    path: "/Contact",
+    element: <Contact></Contact>,
+  },
+  {
+    path: "/Login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/Register",
+    element: <Register></Register>,
   },
 ]);
 
