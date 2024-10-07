d299388ed568b17edd0887832174631fb8d44fe9
Shariful4447 <shariful.islam4447@gmail.com>
2024-10-08 01:43:33 +0200

    global sidebar function created accroding to role

--- src/utils/sidebarItemsGenerator.tsx
+++ src/utils/sidebarItemsGenerator.tsx
@@ -1,17 +1,13 @@
-import { ReactNode } from "react";
+import { NavLink } from "react-router-dom";
+import { TSidebarItem, TUserPath } from "../types";
 
-type TSidebarItem = {
-  key: string;
-  label: ReactNode;
-  children?: TSidebarItem[];
-};
-export const sidebarItemsGenerator = (items: TSidebarItem[]) => {
+export const sidebarItemsGenerator = (items: TUserPath[], role) => {
   const sidebarItems = items.reduce(
     (acc: TSidebarItem[], item) => {
       if (item.path && item.name) {
         acc.push({
           key: item.name,
-          label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+          label: <NavLink to={`/${role}/${item.path}`}>{item.name}</NavLink>,
         });
       }
 
@@ -21,7 +17,9 @@ export const sidebarItemsGenerator = (items: TSidebarItem[]) => {
           label: item.name,
           children: item.children.map((child) => ({
             key: child.name,
-            label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+            label: (
+              <NavLink to={`/${role}/${child.path}`}>{child.name}</NavLink>
+            ),
           })),
         });
       }
