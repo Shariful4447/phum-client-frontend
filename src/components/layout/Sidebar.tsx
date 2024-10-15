43af0ca45264f0d5cbc3f2da674381abfc887936
Shariful4447 <shariful.islam4447@gmail.com>
2024-10-15 23:34:54 +0200

    unique sidebar based on role started

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,7 +3,15 @@ import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
 import { adminPaths } from "../../routes/admin.routes";
 
 const { Sider } = Layout;
+
+const role = {
+  ADMIN: "admin ",
+  FACULTY: "faculty",
+  STUDENT: "student",
+};
 const Sidebar = () => {
+  const role = "admin";
+  let sidebarItems;
   return (
     <Sider breakpoint="lg" collapsedWidth="0">
       <div
