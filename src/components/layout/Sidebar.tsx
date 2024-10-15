2d5706b00e58fb26844a747ef9cb1db01997128a
Shariful4447 <shariful.islam4447@gmail.com>
2024-10-15 23:46:14 +0200

    unique sidebar based on role done but need to set it according to path url

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,17 +1,37 @@
 import { Layout, Menu } from "antd";
 import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
 import { adminPaths } from "../../routes/admin.routes";
+import { facultyPaths } from "../../routes/faculty.routes";
+import { studentPaths } from "../../routes/student.routes";
 
 const { Sider } = Layout;
 
-const role = {
+const userRole = {
   ADMIN: "admin ",
   FACULTY: "faculty",
   STUDENT: "student",
 };
 const Sidebar = () => {
-  const role = "admin";
+  const role = "faculty";
   let sidebarItems;
+  switch (role) {
+    case userRole.ADMIN:
+      sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
+
+      break;
+    case userRole.FACULTY:
+      sidebarItems = sidebarItemsGenerator(facultyPaths, userRole.FACULTY);
+
+      break;
+
+    case userRole.STUDENT:
+      sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDENT);
+
+      break;
+
+    default:
+      break;
+  }
   return (
     <Sider breakpoint="lg" collapsedWidth="0">
       <div
@@ -29,7 +49,7 @@ const Sidebar = () => {
         theme="dark"
         mode="inline"
         defaultSelectedKeys={["4"]}
-        items={sidebarItemsGenerator(adminPaths, "admin")}
+        items={sidebarItems}
       />
     </Sider>
   );
