f59558402a49c777c6e8d7172010f2022bd9b1d7
Shariful4447 <shariful.islam4447@gmail.com>
2024-10-22 01:22:00 +0200

    custom api setup

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,22 @@
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+
+const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api/v1" }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (userInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+  }),
+});
