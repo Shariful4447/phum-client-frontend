cf0ebe49baddba91d7f2f1c27e7b80ce7d98ff50
Shariful4447 <shariful.islam4447@gmail.com>
2024-10-29 21:47:20 +0100

    form data get in console success

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,22 @@
+import { Button } from "antd";
 import { useForm } from "react-hook-form";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="id">ID:</label>
-        <input type="text" id="id"></input>
+        <input type="text" id="id" {...register("id")}></input>
       </div>
       <div>
         <label htmlFor="password">PASSWORD:</label>
-        <input type="text" id="password"></input>
+        <input type="text" id="password" {...register("password")}></input>
       </div>
+      <Button htmlType="submit">Login</Button>
     </form>
   );
 };
