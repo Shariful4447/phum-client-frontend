8bc1dee75b26457161f4341fa2989a95ab5fa404
Shariful4447 <shariful.islam4447@gmail.com>
2024-10-31 20:38:18 +0100

    set user credentials using dispatch in redux

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,27 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/features/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
 
 const Login = () => {
+  const dispatch = useAppDispatch();
   const { register, handleSubmit } = useForm({
     defaultValues: {
       id: "A-0001",
       password: "ami123457",
     },
   });
-  const [login, { data, error }] = useLoginMutation();
-  console.log("data =>", data);
-  console.log("error =>", error);
+  const [login, { error }] = useLoginMutation();
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const userInfo = {
       id: data.id,
       password: data.password,
     };
-    login(userInfo);
+    const res = await login(userInfo).unwrap();
+    dispatch(setUser({ user: {}, token: res.data.accessToken }));
+    console.log(res);
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
