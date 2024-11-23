28b0f733a22f77810f66c52c88a66553f3499167
Shariful4447 <shariful.islam4447@gmail.com>
2024-11-23 23:12:25 +0100

    toast introduce for log in and log out

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch } from "../redux/features/hooks";
 import { setUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -18,16 +19,22 @@ const Login = () => {
   const [login, { error }] = useLoginMutation();
 
   const onSubmit = async (data) => {
-    const userInfo = {
-      id: data.id,
-      password: data.password,
-    };
-    const res = await login(userInfo).unwrap();
-    const user = verifyToken(res.data.accessToken);
-    console.log(user);
+    toast.loading("Logging in...");
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken);
+      console.log(user);
 
-    dispatch(setUser({ user: user, token: res.data.accessToken }));
-    navigate(`/${user.role}/dashboard`);
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success("User logged in successfully");
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
