45a7161a2e0cdf41fade5fe8048cb179d19bc482
Shariful4447 <shariful.islam4447@gmail.com>
2024-12-31 23:20:29 +0100

    error in the login credientials

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { RootState } from "../store";
+import { RootState } from "../features/store";
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1/",
   credentials: "include",
@@ -14,14 +14,12 @@ const baseQuery = fetchBaseQuery({
 });
 
 const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = baseQuery(args, api, extraOptions);
+  const result = await baseQuery(args, api, extraOptions);
+  console.log(result);
 };
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api/v1",
-    credentials: "include",
-  }),
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
