5900cbb8ef799ebf2fe8978abaf617896fedfa5b
Shariful4447 <shariful.islam4447@gmail.com>
2024-12-31 23:36:07 +0100

    logged in successfully but redirect issue

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../features/store";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1/",
+  baseUrl: "http://localhost:3000/api/v1/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
